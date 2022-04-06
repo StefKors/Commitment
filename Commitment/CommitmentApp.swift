e4f65c1861da92ea8a7b653c5597921f60f8cb68
Stef Kors <stef.kors@gmail.com>
2022-04-06 12:08:48 +0200

    Initial Commit

---
+++ Commitment/CommitmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommitmentApp.swift
+//  Commitment
+//
+//  Created by Stef Kors on 06/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct CommitmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
