b383fdcfd152797ba0a949c58d61e9ade378d1f4
Stef Kors <stef@beamapp.co>
2023-01-10 17:45:42 +0100

    remove

--- Commitment/ContentView.swift
+++ Commitment/ContentView.swift
@@ -11,20 +11,20 @@ struct ContentView: View {
     @EnvironmentObject var git: GitClient
     @State private var message: String = ""
     @StateObject private var diffstate: DiffState = DiffState()
-
+    
     var body: some View {
         VStack {
             HStack {
                 TextEditorView(message: $message)
                     .onSubmit { handleSubmit() }
-
+                
                 Button(action: { handleSubmit() }) {
                     Text("Commit")
                 }
                 .buttonStyle(.borderedProminent)
                 .disabled(message.isEmpty)
             }
-
+            
             DiffView(diffs: diffstate.diffs)
                 .task {
                     self.diffstate.diffs = git.diff()
@@ -32,11 +32,13 @@ struct ContentView: View {
         }
         .scenePadding()
     }
-
+    
     func handleSubmit() {
-        git.commit(message: message)
-        message = ""
-        self.diffstate.diffs = git.diff()
+        withAnimation {
+            git.commit(message: message)
+            message = ""
+            self.diffstate.diffs = git.diff()
+        }
     }
 }
 
