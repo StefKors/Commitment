9fe5de06194d7a201186e3048aad4a8148c417e3
Stef Kors <stef.kors@gmail.com>
2023-02-27 01:19:21 +0100

    test another commit

--- Commitment/Models/Shell+Git.swift
+++ Commitment/Models/Shell+Git.swift
@@ -56,12 +56,12 @@ extension Shell {
 
     func commit(title: String, message: String) async throws {
         try await self.add()
-        try await self.run(.git, ["commit", "-m", "\(title)", "-m", "\"\(message)\""])
+        try await self.run(.git, ["commit", "-m", title, "-m", "\"\(message)\""])
     }
 
     func commit(message: String) async throws {
         try await self.add()
-        try await self.run(.git, ["commit", "-m", "\"\(message)\""])
+        try await self.run(.git, ["commit", "-m", message])
     }
 
     func push() async throws -> String {
