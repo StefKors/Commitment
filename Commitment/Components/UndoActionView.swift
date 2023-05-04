a3330278697f66310dd05bb07f7468ad7a14f1f4
Stef Kors <stef.kors@gmail.com>
2023-05-04 16:04:11 +0200

    Specifiy unit in relative dates

--- Commitment/Components/UndoActionView.swift
+++ Commitment/Components/UndoActionView.swift
@@ -13,7 +13,7 @@ struct UndoActionView: View {
     var body: some View {
         HStack {
             VStack(alignment: .leading) {
-                Text("\(action.type.rawValue.capitalized)ed ") + Text("\(action.createdAt.formatted(.relative(presentation: .named)))")
+                Text("\(action.type.rawValue.capitalized)ed ") + Text("\(action.createdAt.formatted(.relative(presentation: .named, unitsStyle: .wide)))")
                 if let subtitle = action.subtitle {
                     Text(subtitle)
                         .foregroundColor(.secondary)
