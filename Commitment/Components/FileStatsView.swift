d8064fc2051451a6ce80d95a9e9e4a934a0190bc
Stef Kors <stef.kors@gmail.com>
2023-09-28 18:38:33 +0200

    Add View switcher

--- Commitment/Components/FileStatsView.swift
+++ Commitment/Components/FileStatsView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct FileStatsView: View {
     let stats: GitFileStats?
 
+    @AppStorage("SideBySideView") private var sideBySide: Bool = false
+
+    @State private var selection = 0
+
     var body: some View {
         if let stats {
             HStack {
@@ -29,6 +33,35 @@ struct FileStatsView: View {
                 .fontDesign(.monospaced)
                 .shadow(radius: 4, y: 2)
                 .padding()
+
+//                GroupBox {
+                    HStack(spacing: 8) {
+                        Picker("Choose view style?", selection: $selection) {
+                            Image(systemName: "rectangle.split.2x1.fill").tag(0)
+                            Image(systemName: "rectangle.split.1x2.fill").tag(1)
+                        }
+                        .pickerStyle(.segmented)
+                            .labelsHidden()
+                            .frame(width: 100)
+                    }
+                    .onChange(of: selection, initial: false, { oldValue, newValue in
+                        if selection == 0 {
+                            sideBySide = true
+                        } else {
+                            sideBySide = false
+                        }
+                    })
+//                }
+                .background(.thinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 4))
+                .fontDesign(.monospaced)
+                .shadow(radius: 4, y: 2)
+                .padding()
+                .task {
+                    selection = sideBySide ? 0 : 1
+                }
+
+
             }
         }
     }
